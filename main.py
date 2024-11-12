587af0c8842bc9b7d8e6ab5a43577be5c4afbb53
Jcardenasac <jcardenasac@unal.edu.co>
2024-11-11 20:20:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class Auto:
     def verificarIntegridad(self):
         if self.registro == self.motor.registro:
             for asiento in self.asientos:
-                if asiento is not None and asiento.registro != self.registro:
+                if asiento is not None and asiento.registro != self.motor.registro:
                     return "Las piezas no son originales"
             return "Auto original"
         return "Las piezas no son originales"
