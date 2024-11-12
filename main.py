b719d04f65a8a19121aff4177d136725881a60f1
Jcardenasac <jcardenasac@unal.edu.co>
2024-11-11 21:30:55 -0500

    mm.py

--- main.py
+++ main.py
@@ -1,51 +1,53 @@
 class Asiento:
-    def __init__(self, color, precio, registro):
-        self.color = color
-        self.precio = precio
-        self.registro = registro
+     def __init__(self,color,precio,registro):
+         self.color = color
+         self.precio = precio
+         self.registro = registro
 
-    def cambiarColor(self, color):
-        if color in ("verde", "negro", "amarillo", "rojo", "blanco"):
-            self.color = color
-
-
-class Motor:
-    def __init__(self, numeroCilindros, tipo, registro):
-        self.numeroCilindros = numeroCilindros
-        self.tipo = tipo  
-        self.registro = registro
-
-    def cambiarRegistro(self, nuevoregistro):
-        self.registro = nuevoregistro
-    def asignarTipo(self, tipo):
-        if tipo == "electrico" or tipo == "gasolina":
-            self.tipo = tipo
-
+    
+     def cambiarColor(self, color):
+        if color == "rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco":
+             self.color = color
 
 class Auto:
-    def __init__(self, modelo, precio, asientos, marca, motor, color, registro):
+    
+     cantidadCreados = 0
+     
+     def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
         self.asientos = asientos
         self.marca = marca
         self.motor = motor
-        self.color = color
         self.registro = registro
-      
 
-    def cantidadAsientos(self):
-        contador = 0
+     def cantidadAsientos(self):
+        cantidad = 0
         for elemento in self.asientos:
-            if isinstance(elemento, Asiento):
-                contador += 1
-        return contador
+            if type(elemento) == Asiento:
+                cantidad += 1
+        return (cantidad)
+     
+     def verificarIntegridad(self):
+        if self.registro == self.motor.registro:
+            for elemento in self.asientos:
+                if elemento.registro != self.registro:
+                    return("Las piezas no son originales")
+                else:
+                    return("Auto original")
+        return("Las piezas no son originales")
 
-    def verificarIntegridad(self):
-        if self.registro != self.motor.registro:
-           return "Las piezas no son originales"
-    
-        for asiento in self.asientos:
-           if asiento is not None and asiento.registro != self.registro:
-               return "Las piezas no son originales"
-    
-        return "Auto original"
+class Motor:
+     def __init__(self,numeroCilindros,tipo,registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+     
+     def cambiarRegistro(self,registro):
+         self.registro = registro
+
+     def asignarTipo(self,tipo):
+         if tipo == "electrico" or tipo == "gasolina":
+             self.tipo = tipo
+
+     
